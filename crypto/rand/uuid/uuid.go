b0c9c74a9f57894a6c58304287b8d5befafa5c5f
Kenneth Gibson <kengibson1111@gmail.com>
2016-05-23 11:05:23 -0600

    added more sample comments.

--- crypto/rand/uuid/uuid.go
+++ crypto/rand/uuid/uuid.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// like the basic sample.
 	b := make([]byte, 16)
 	_, err := rand.Read(b)
 	if err != nil {
@@ -13,11 +14,16 @@ func main() {
 		return
 	}
 
-	// this make sure that the 13th character is "4"
+	// 1st half of byte 7 is "4". So the first OR guarantees
+	// that the 1st half of byte 7 is at least 4. The AND guarantees that the
+	// max value of byte 7 is 4. Always 4.
 	b[6] = (b[6] | 0x40) & 0x4F
-	// this make sure that the 17th is "8", "9", "a", or "b"
+	// 1st half of byte 9 is "8", "9", "a", or "b". The first
+	// OR guarantees that the 1st half of byte 9 is at least 8. The AND
+	// guarantees that the max value of byte 9 is b.
 	b[8] = (b[8] | 0x80) & 0xBF
-
+	
+	//done
 	uuid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 	fmt.Println("UUID: ", uuid)
 }
