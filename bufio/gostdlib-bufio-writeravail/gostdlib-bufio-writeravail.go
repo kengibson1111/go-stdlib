767cf498491368d06aff7484e301d9553e24f77c
Ken Gibson <kengibson1111@gmail.com>
2023-07-17 23:18:40 +0000

    Workspace and module changes

---
+++ bufio/gostdlib-bufio-writeravail/gostdlib-bufio-writeravail.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	w := bufio.NewWriter(os.Stdout)
+
+	fmt.Fprint(w, "Hello, ")
+	fmt.Fprint(w, "world!\n")
+
+	w.Flush() // Don't forget to flush!
+}
