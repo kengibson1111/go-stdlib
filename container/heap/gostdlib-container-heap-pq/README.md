65dae96ff19da9b3fadd83bda3a6d95f48b34412
Ken Gibson <kengibson1111@gmail.com>
2023-08-30 12:50:37 +0000

    Workspace and module changes

---
+++ container/heap/gostdlib-container-heap-pq/README.md
@@ -0,0 +1,3 @@
+# container/heap - priority queue
+
+This is an example of an priority-queue heap using the heap interface. The example includes a struct and what happens with the queue when struct values are updated.
--- container/heap/gostdlib-container-heap-pq/gostdlib-container-heap-pq.go
+++ container/heap/gostdlib-container-heap-pq/gostdlib-container-heap-pq.go
@@ -8,7 +8,7 @@ import (
 
 // An Item is something we manage in a priority queue.
 type Item struct {
-	value string // The value of the item; arbitrary.
+	value    string // The value of the item; arbitrary.
 	priority int    // The priority of the item in the queue.
 	// The index is needed by update and is maintained by the heap.Interface methods.
 	index int // The index of the item in the heap.
@@ -90,4 +90,6 @@ func main() {
 		item := heap.Pop(&pq).(*Item)
 		fmt.Printf("%.2d:%s ", item.priority, item.value)
 	}
+
+	fmt.Println()
 }
