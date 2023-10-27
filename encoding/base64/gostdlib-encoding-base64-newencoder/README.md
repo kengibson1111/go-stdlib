25dbd76bc0d65ed59c86fb0741420c1afeee3e0c
Ken Gibson <kengibson1111@gmail.com>
2023-10-27 14:33:32 +0000

    Workspace and module changes

---
+++ encoding/base64/gostdlib-encoding-base64-newencoder/README.md
@@ -0,0 +1,3 @@
+# encoding/base64 - newencoder
+
+This streams a string to a base64 value.
--- encoding/base64/gostdlib-encoding-base64-newencoder/gostdlib-encoding-base64-newencoder.go
+++ encoding/base64/gostdlib-encoding-base64-newencoder/gostdlib-encoding-base64-newencoder.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/base64"
+	"fmt"
 	"os"
 )
 
@@ -9,8 +10,11 @@ func main() {
 	input := []byte("foo\x00bar")
 	encoder := base64.NewEncoder(base64.StdEncoding, os.Stdout)
 	encoder.Write(input)
+
 	// Must close the encoder when finished to flush any partial blocks.
 	// If you comment out the following line, the last partial block "r"
 	// won't be encoded.
 	encoder.Close()
+
+	fmt.Println()
 }
