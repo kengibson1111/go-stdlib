daef1af7a2b68d66db95e9cf391c04ae846f049e
Ken Gibson <kengibson1111@gmail.com>
2024-02-21 15:15:54 +0000

    Workspace and module changes

---
+++ io/gostdlib-io-multireader/README.md
@@ -0,0 +1,3 @@
+# io - multireader
+
+This shows how to use MultiReader.
---
+++ io/gostdlib-io-multireader/gostdlib-io-multireader.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	r1 := strings.NewReader("first reader ")
+	r2 := strings.NewReader("second reader ")
+	r3 := strings.NewReader("third reader\n")
+
+	r := io.MultiReader(r1, r2, r3)
+
+	_, err := io.Copy(os.Stdout, r)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
