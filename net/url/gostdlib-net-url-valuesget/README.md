7d811f43ff6e7ee857c7435385e83cc55c3f51ff
Ken Gibson <kengibson1111@gmail.com>
2024-08-10 14:20:00 +0000

    Workspace and module changes

---
+++ net/url/gostdlib-net-url-valuesget/README.md
@@ -0,0 +1,3 @@
+# net/url - valuesget
+
+This shows how to use Values.Get.
---
+++ net/url/gostdlib-net-url-valuesget/gostdlib-net-url-valuesget.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+func main() {
+	v := url.Values{}
+	v.Add("cat sounds", "meow")
+	v.Add("cat sounds", "mew")
+	v.Add("cat sounds", "mau")
+
+	fmt.Printf("%q\n", v.Get("cat sounds"))
+	fmt.Printf("%q\n", v.Get("dog sounds"))
+}
