c1bdc33176e1cecae31e016aa4e7d5b634951201
Ken Gibson <kengibson1111@gmail.com>
2025-02-03 15:25:52 +0000

    Workspace and module changes

---
+++ strconv/gostdlib-strconv-formatfloat/README.md
@@ -0,0 +1,3 @@
+# strconv - formatfloat
+
+This example shows how to use FormatFloat.
---
+++ strconv/gostdlib-strconv-formatfloat/gostdlib-strconv-formatfloat.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	v := 3.1415926535
+
+	s32 := strconv.FormatFloat(v, 'E', -1, 32)
+	fmt.Printf("%T, %v\n", s32, s32)
+
+	s64 := strconv.FormatFloat(v, 'E', -1, 64)
+	fmt.Printf("%T, %v\n", s64, s64)
+
+	// fmt.Println uses these arguments to print floats
+	fmt64 := strconv.FormatFloat(v, 'g', -1, 64)
+	fmt.Printf("%T, %v\n", fmt64, fmt64)
+}
